b0a4c46cc46baf052fd280b03cd95b5c71d3e445
Adithya-Nath <adithyanathal@gmail.com>
2024-03-17 12:35:51 +0530

    commit

---
+++ ui-react/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Navbar from "./components/Public/Navbar";
+
+const App = () => {
+    return(
+        <div className="text-blue-400 font-bold underline">App</div>
+    )
+}
+export default App
---
+++ ui-react/src/components/User/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Navbar from "./components/Public/Navbar";
+
+const Home = () => {
+    return(
+        <div className="text-blue-400 font-bold underline">home</div>
+    )
+}
+export default Home;
